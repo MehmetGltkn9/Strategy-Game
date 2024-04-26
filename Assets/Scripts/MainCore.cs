4478bfe3abf33bf7bcdf42ba200fbc3bb19805c2
MehmetGltkn9 <mehmetgltkn943@gmail.com>
2024-04-26 12:41:57 +0300

    Update MainCore.cs

--- Assets/Scripts/MainCore.cs
+++ Assets/Scripts/MainCore.cs
@@ -17,29 +17,46 @@ public class MainCore : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
+        if (Input.GetMouseButtonDown(0)) {   // Mouse button "0" clicked
+
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 100.0f))
-            {
-                if(currentSelectedObject != null)
-                {
+
+            if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Building","Unit"))) {  // Ray got hit to a building or a unit
+
+                if(currentSelectedObject != null) { // There is already a selected object
+
                     MeshRenderer mrOld = currentSelectedObject.GetComponent<MeshRenderer>();
-                    if (mrOld != null)
-                    {
-                        mrOld.material = matUnselected;
+
+                    if (mrOld != null) { // Current selected object has a meshrenderer
+                        mrOld.material = matUnselected; // Change material to unselected
                     }
+
                 }
 
-                currentSelectedObject = hit.transform.gameObject;
-                MeshRenderer mr = currentSelectedObject.GetComponent<MeshRenderer>();
+                currentSelectedObject = hit.transform.gameObject; // Get the recently selected object
+                MeshRenderer mr = currentSelectedObject.GetComponent<MeshRenderer>(); 
 
-                if (mr != null)
-                {
-                    mr.material = matSelected;
+                if (mr != null) { // Recently selected object has a meshrenderer
+                    mr.material = matSelected; // Change material to selected
+                }
+
+            }
+
+            else {  // Ray did not hit to a building or a unit
+                
+                if (currentSelectedObject != null) { // There is already a selected object 
+
+                    MeshRenderer mrOld = currentSelectedObject.GetComponent<MeshRenderer>();
+
+                    if (mrOld != null) { // Current selected object has a meshrenderer
+                        mrOld.material = matUnselected; // Change material to unselected
+                    }
+
+                    currentSelectedObject = null;
                 }
             }
+
         }
     }
 }
